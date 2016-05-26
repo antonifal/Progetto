33a4a86fdf5c06c0e2882efcfc261d1c849a2337
Antonio <antonio.falcone87@gmail.com>
2016-05-26 18:39:53 +0200

    pericolo scampato

--- connection.class.php
+++ connection.class.php
@@ -6,34 +6,30 @@ class connection
   private $password = "";
   private $connection="";
   private $result="";
-  private $q="SELECT * FROM codici";
-
-  public function __construct()
-  {
-    $this->connect();
-    $this->send_query($this->q);
-    $this->get_resource_array();
-    $this->disconnect();
-  }
-
+  private $q="SELECT * FROM mesi";
+//  public function __construct()
+//  {
+//    $this->connect();
+//    $this->send_query($this->q);
+  //  $this->get_resource_multid_array();
+//    $this->get_resource_array();
+//    $this->disconnect();
+//  }
   public function connect()
   {
       $this->connection = mysql_connect($this->host, $this->user, $this->password)or die("Connession non riuscita: " . mysql_error());
   }
-
   public function disconnect()
   {
     mysql_close($this->connection);
   }
-
-  public function send_query($q)
+  public function send_query($q,$db)
   {
-    mysql_select_db("codicefiscale");
+    mysql_select_db($db);
     $this->result = mysql_query($q) or die("Query non valida: " . mysql_error());
   //  mysql_free_result($this->res);
   }
-
-  public function get_resource_array()
+  public function get_resource_multid_array()
   {
     $i=0;
     while ($riga = mysql_fetch_array($this->result, MYSQL_NUM))
@@ -42,11 +38,18 @@ class connection
       $res_array[$i]=$riga;
       $i++;
     }
-    print_r($res_array);
+    return $res_array;
   }
-
-
+  public function get_resource_array()
+  {
+    $i=0;
+    while ($riga = mysql_fetch_array($this->result, MYSQL_NUM))
+    {
+      $res_array[$i]=implode(";", $riga);
+      $i++;
+    }
+    return $res_array;
   }
-
-new connection;
+}
+//$a=new connection;
 ?>
--- place.class.php
+++ place.class.php
@@ -1,87 +1,82 @@
 <?php
 require_once('read_data.class.php');
-
+require_once('connection.class.php');
 class place
 {
 	private $comune;
 	private $provincia;
 	private $code;
 	private $msg_error=array();
-
 	public function __construct()
 	{
 		$this->set_comune();
 		$this->set_provincia();
 		$this->check_place() ;
 	}
-
 	private function set_comune ()
 	{
 		echo "\n\nInserire comune: ";
 		$this->comune=trim(fgets(STDIN));
 	}
-
 	private function set_provincia()
 	{
-		echo "\n\nInserire Provincia: \n\n";
+		echo "\n\nInserire Provincia: ";
 		$this->provincia=trim(fgets(STDIN));
 	}
-
 	private function check_place ()
-	{	
-		$search=" ";
-		$replace="";
+	{
+		$data_obj=new connection;
+		$data_obj->connect();
+		$data_obj->send_query("SELECT * FROM codici","codicefiscale");
+		$data=$data_obj->get_resource_array();
+		$data_obj->disconnect();
+
 		$town=str_replace(" ","",$this->comune);
 		if (strlen($this->comune)<3)
 		{
 			array_push($this->msg_error,'town');
 		}
-
 		if(!ctype_alpha($town))
 		{
 			array_push($this->msg_error,'town2');
 		}
-
 		if (strlen($this->provincia)!=2)
 		{
 			array_push($this->msg_error,'district');
 		}
-
 		if(!ctype_alpha($this->provincia))
 		{
 			array_push($this->msg_error,'district2');
 		}
-
-		$data=new read_data('codici_comuni_italiani.txt');
+	//	$data=new read_data('codici_comuni_italiani.txt');
 		$district_found=false;
 		$town_found=false;
 		$indx_district=1;
 		$indx_town=0;
-
-		for($i=0;$i<count($data->get_resource());$i++)
+		//for($i=0;$i<count($data->get_resource());$i++)
+		for($i=0;$i<count($data);$i++)
 		{
-			$res=explode(';',$data->get_resource()[$i]);
-
+			//$res=explode(';',$data->get_resource()[$i]);
+			$res=explode(';',$data[$i]);
+			//print_r($res[0]);
+		//	exit;
 			if($res[1]==strtoupper($this->comune))
 			{
 				$town_found=true;
 				$indx_town=$i;
 			}
-
 			if(trim($res[2])==strtoupper($this->provincia))
 			{
 				$district_found=true;
 				$indx_district=$i;
 			}
-
 			if($town_found==true and $district_found==true and $indx_district==$indx_town)
 			{
-				$i=count($data->get_resource());
+				//$i=count($data->get_resource());
+				$i=count($data);
 				$this->code=$res[0];
 			}
-
 		}
-
 		if($town_found==false)
 		{
 			array_push($this->msg_error,'town3');
@@ -90,29 +85,23 @@ class place
 		{
 			array_push($this->msg_error,'district3');
 		}
-
 	}
-
 	public function get_comune()
  	{
 		return $this->comune;
 	}
-
  	public function get_provincia()
  	{
 	 	return $this->provincia;
 	}
-
 	public function get_placecode()
 	{
 		return $this->code;
 	}
-
 	public function get_msg_error()
 	{
 		return $this->msg_error;
 	}
-
 }
 //$c=new place;
 ?>
